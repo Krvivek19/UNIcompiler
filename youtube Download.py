85c7a76205b9a93eb90eada0a2c091b15b57834c
Krvivek19 <111839769+Krvivek19@users.noreply.github.com>
2022-08-24 21:02:58 +0530

    Update youtube Download.py

--- youtube Download.py
+++ youtube Download.py
@@ -8,13 +8,13 @@ print("*********************Video Title************************")
 #get Video Title
 print(my_video.title)
 
-print("********************Tumbnail Image***********************")
+print("**********************:-Tumbnail Image:-*************************")
 #get Thumbnail Image
 print(my_video.thumbnail_url)
-
+print()
 print("********************Download video*************************")
 #get all the stream resolution for the 
-
+print()
 
 #set stream resolution
 my_video = my_video.streams.get_highest_resolution()
